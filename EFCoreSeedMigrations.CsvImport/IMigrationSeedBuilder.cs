619695097382bce14ba69e3358743d8c0d855805
nazimkov <vyacheslav.nazimkov@gmail.com>
2019-01-14 20:55:09 +0200

    Create IMigrationSeedBuilder.cs

---
+++ EFCoreSeedMigrations.CsvImport/IMigrationSeedBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreSeedMigrations.CsvImport
+{
+    public interface IMigrationSeedBuilder
+    {
+        MigrationBuilder MigrationBuilder { get; set; }
+
+        void InsertData<TModel>(string filePath);
+        void DeleteData<TModel>(string filePath);
+        void UpdateData<TModel>(string filePath);
+    }
+}
