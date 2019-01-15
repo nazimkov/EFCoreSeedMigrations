1f9a18c379b45aa9f58bdf673833e59431136a79
nazimkov <vyacheslav.nazimkov@gmail.com>
2019-01-15 18:18:46 +0200

    Move to separate class

---
+++ EFCoreSeedMigrations.CsvImport/MigrationSeedBuilder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreSeedMigrations.CsvImport
+{
+    public class MigrationSeedBuilder : IMigrationSeedBuilder
+    {
+        private readonly ICsvSeedDataReader _csvSeedDataReader;
+
+        public MigrationSeedBuilder(ICsvSeedDataReader csvSeedDataReader)
+        {
+            _csvSeedDataReader = csvSeedDataReader;
+        }
+
+        public MigrationBuilder MigrationBuilder { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void DeleteData<TModel>(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InsertData<TModel>(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateData<TModel>(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
