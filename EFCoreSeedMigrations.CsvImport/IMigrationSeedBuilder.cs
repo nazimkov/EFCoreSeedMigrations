b64580b030003d70c610e99d2e3c33c264a53cf8
nazimkov <vyacheslav.nazimkov@gmail.com>
2019-01-15 18:17:46 +0200

    Update IMigrationSeedBuilder.cs

--- EFCoreSeedMigrations.CsvImport/IMigrationSeedBuilder.cs
+++ EFCoreSeedMigrations.CsvImport/IMigrationSeedBuilder.cs
@@ -10,4 +10,31 @@ namespace EFCoreSeedMigrations.CsvImport
         void DeleteData<TModel>(string filePath);
         void UpdateData<TModel>(string filePath);
     }
+
+    public class MigrationSeedBuilder : IMigrationSeedBuilder
+    {
+        private readonly ICsvSeedDataReader _csvSeedDataReader;
+
+        public MigrationSeedBuilder(ICsvSeedDataReader csvSeedDataReader)
+        {
+            _csvSeedDataReader = csvSeedDataReader;
+        }
+
+        public MigrationBuilder MigrationBuilder { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void DeleteData<TModel>(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InsertData<TModel>(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateData<TModel>(string filePath)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
